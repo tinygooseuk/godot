710d60616e412db2169701eb7de236064c2287e0
Joe Flood <joe@tinygoose.co.uk>
2019-11-12 13:34:38 +0000

    Add new build script for Mono (careful, you must run each command separately!)

--- build_engine.bat
+++ build_engine.bat
@@ -1,8 +1,17 @@
 @echo off
 
-scons p=windows vcproj=yes -j16
+echo Generating Mono Glue build
+scons p=windows tools=yes module_mono_enabled=yes mono_glue=no -j16
 if %errorlevel%==1 goto error
 
+echo Generating Mono glue code
+bin\godot.windows.tools.64.mono.exe --generate-mono-glue modules/mono/glue 
+if %errorlevel%==1 goto error
+
+echo Building mono editor
+scons p=windows tools=yes module_mono_enabled=yes mono_glue=yes vcproj=yes -j16
+
+if %errorlevel%==1 goto error
 exit /B
 
 error:
