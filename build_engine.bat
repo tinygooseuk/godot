e1e54f19ef6fa914b08db1b8d2aa20dc93bb6af4
Joe Flood <joe@tinygoose.co.uk>
2019-11-14 00:15:35 +0000

    Fix scons quitting batch file

--- build_engine.bat
+++ build_engine.bat
@@ -1,15 +1,15 @@
 @echo off
 
 echo Generating Mono Glue build
-scons p=windows tools=yes module_mono_enabled=yes mono_glue=no -j16
+call scons p=windows tools=yes module_mono_enabled=yes mono_glue=no -j16
 if %errorlevel%==1 goto error
 
 echo Generating Mono glue code
-bin\godot.windows.tools.64.mono.exe --generate-mono-glue modules/mono/glue 
+call bin\godot.windows.tools.64.mono.exe --generate-mono-glue modules/mono/glue 
 if %errorlevel%==1 goto error
 
 echo Building mono editor
-scons p=windows tools=yes module_mono_enabled=yes mono_glue=yes vcproj=yes -j16
+call scons p=windows tools=yes module_mono_enabled=yes mono_glue=yes vcproj=yes -j16
 
 if %errorlevel%==1 goto error
 exit /B
