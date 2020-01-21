6dfffe651ff45540e3bf817aa00a855500255283
Joe Flood <joe@tinygoose.co.uk>
2020-01-21 23:42:58 +0000

    Update build scripts

--- build_engine.bat
+++ build_engine.bat
@@ -1,15 +1,31 @@
 @echo off
 
-echo Generating Mono Glue build
+echo **************************************************
+echo Building vanilla editor with mono module...
+echo **************************************************
+
 call scons p=windows tools=yes module_mono_enabled=yes mono_glue=no -j16
 if %errorlevel%==1 goto error
 
-echo Generating Mono glue code
+
+
+echo **************************************************
+echo Generating Mono type data...
+echo **************************************************
+
 call bin\godot.windows.tools.64.mono.exe --generate-mono-glue modules/mono/glue 
 if %errorlevel%==1 goto error
 
-echo Building mono editor
+
+
+echo **************************************************
+echo Building Godot Engine...
+echo **************************************************
+
 call scons p=windows tools=yes module_mono_enabled=yes mono_glue=yes vsproj=yes -j16
+
+
+
 
 if %errorlevel%==1 goto error
 exit /B
--- build_templates.bat
+++ build_templates.bat
@@ -2,15 +2,29 @@
 
 set VERSION=3.2.beta.mono
 
+echo **************************************************
+echo Preparing to build export templates for Win64...
+echo **************************************************
+
 rd /s/q "%APPDATA%\Godot\templates\%VERSION%"
 mkdir "%APPDATA%\Godot\templates\%VERSION%"
 
-echo Building Debug...
+
+
+echo **************************************************
+echo Building DEBUG template for Win64...
+echo **************************************************
+
 call scons p=windows tools=no module_mono_enabled=yes mono_glue=yes target=release_debug bits=64 -j16
 copy bin\godot.windows.opt.debug.64.mono.exe "%APPDATA%\Godot\templates\%VERSION%\windows_64_debug.exe"
 robocopy bin\data.mono.windows.64.release_debug "%APPDATA%\Godot\templates\%VERSION%\data.mono.windows.64.release_debug" /E /is /it
 
-echo Building Release...
+
+
+echo **************************************************
+echo Building RELEASE template for Win64...
+echo **************************************************
+
 call scons p=windows tools=no module_mono_enabled=yes mono_glue=yes target=release bits=64 -j16
 copy bin\godot.windows.opt.64.mono.exe "%APPDATA%\Godot\templates\%VERSION%\windows_64_release.exe"
 robocopy bin\data.mono.windows.64.release "%APPDATA%\Godot\templates\%VERSION%\data.mono.windows.64.release" /E /is /it
