8c0ce565ab6f10b97a5f6f117e2ff1463cdbd12e
Joe Flood <joe@tinygoose.co.uk>
2020-01-30 18:18:23 +0000

    Tweak TinyGoose property macros for clang

--- core/tg_util.h
+++ core/tg_util.h
@@ -26,9 +26,9 @@ public:                                                     \
 #define IMPLEMENT_PROPERTY(cpp_class, gd_type, name)            \
 	ClassDB::bind_method("get_" #name, &cpp_class::get_##name); \
 	ClassDB::bind_method("set_" #name, &cpp_class::set_##name); \
-	ClassDB::add_property(#cpp_class, PropertyInfo(Variant::##gd_type, #name), "set_" #name, "get_" #name);
+	ClassDB::add_property(#cpp_class, PropertyInfo(Variant::gd_type, #name), "set_" #name, "get_" #name);
 
 #define IMPLEMENT_PROPERTY_TYPEHINT(cpp_class, gd_type, type_hint, name) \
 	ClassDB::bind_method("get_" #name, &cpp_class::get_##name);          \
 	ClassDB::bind_method("set_" #name, &cpp_class::set_##name);          \
-	ClassDB::add_property(#cpp_class, PropertyInfo(Variant::##gd_type, #name, PROPERTY_HINT_TYPE_STRING, #type_hint), "set_" #name, "get_" #name);
+	ClassDB::add_property(#cpp_class, PropertyInfo(Variant::gd_type, #name, PROPERTY_HINT_TYPE_STRING, #type_hint), "set_" #name, "get_" #name);
