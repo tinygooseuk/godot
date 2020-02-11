70c19fc1fe9dedabc3fe624b9e5cbfe34166521e
Joe Flood <joe@tinygoose.co.uk>
2020-02-11 22:39:45 +0000

    Fix TGUtil type hinting

--- core/tg_util.h
+++ core/tg_util.h
@@ -15,13 +15,21 @@ constexpr T clamp(T value, T min_v, T max_v) {
 	return max(min_v, min(max_v, value));
 }
 
-#define DECLARE_PROPERTY(cpp_type, name, default_value)     \
-private:                                                    \
-	cpp_type name = default_value;                          \
-                                                            \
-public:                                                     \
-	void set_##name(cpp_type p_##name) { name = p_##name; } \
-	cpp_type get_##name() const { return name; }
+#define DECLARE_PROPERTY(cpp_type, name, default_value)    		\
+	private:                                                    \
+		cpp_type name = default_value;                          \
+																\
+	public:                                                     \
+		void set_##name(cpp_type p_##name) { name = p_##name; } \
+		cpp_type get_##name() const { return name; }
+
+
+#define DECLARE_PUBLIC_PROPERTY(cpp_type, name, default_value) 	\
+	public:                                                    	\
+		cpp_type name = default_value;                          \
+																\
+		void set_##name(cpp_type p_##name) { name = p_##name; } \
+		cpp_type get_##name() const { return name; }
 
 #define IMPLEMENT_PROPERTY(cpp_class, gd_type, name)            \
 	ClassDB::bind_method("get_" #name, &cpp_class::get_##name); \
@@ -31,4 +39,4 @@ public:                                                     \
 #define IMPLEMENT_PROPERTY_TYPEHINT(cpp_class, gd_type, type_hint, name) \
 	ClassDB::bind_method("get_" #name, &cpp_class::get_##name);          \
 	ClassDB::bind_method("set_" #name, &cpp_class::set_##name);          \
-	ClassDB::add_property(#cpp_class, PropertyInfo(Variant::gd_type, #name, PROPERTY_HINT_TYPE_STRING, #type_hint), "set_" #name, "get_" #name);
+	ClassDB::add_property(#cpp_class, PropertyInfo(Variant::gd_type, #name, PROPERTY_HINT_RESOURCE_TYPE, #type_hint), "set_" #name, "get_" #name);
