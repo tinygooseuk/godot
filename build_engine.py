ef5516b74ee7361eda24ab7c3d41fcf8e4b6d31f
Joe Flood <joe@tinygoose.co.uk>
2020-02-22 10:56:51 +0000

    WIP python build script

--- build_engine.py
+++ build_engine.py
@@ -4,6 +4,7 @@ import os
 import shutil
 
 ##### CONSTS #########################
+DEFAULT_COMMAND = "glue"
 VERSION = "3.2.1.rc.mono"
 
 
@@ -31,20 +32,44 @@ def get_godot_tools_exe_name():
     else:
         return "bin\\godot.windows.tools.64.mono.exe"
 
-"linux_x11_64_debug"
-"windows_64_debug"
-
 def get_godot_debug_template_exe_name():
     if os.name == "posix":
         return "bin/godot.x11.opt.debug.64.llvm.mono"
     else:
         return "bin\\godot.windows.opt.debug.64.mono.exe"
 
+def get_godot_debug_data_name():
+    if os.name == "posix":
+        return "bin/data.mono.x11.64.release_debug"
+    else:
+        return "bin\\data.mono.windows.64.release_debug"
+
+
 def get_godot_release_template_exe_name():
     if os.name == "posix":
         return "bin/godot.x11.opt.64.llvm.mono"
     else:
         return "bin\\godot.windows.opt.64.mono.exe"
+
+
+def get_godot_release_data_name():
+    if os.name == "posix":
+        return "bin/data.mono.x11.64.release"
+    else:
+        return "bin\\data.mono.windows.64.release"
+
+
+def get_godot_template_root_name():
+    if os.name == "posix":
+        return "linux_x11"
+    else:
+        return "windows"
+        
+def get_godot_template_exe_extension():
+    if os.name == "posix":
+        return ""
+    else:
+        return ".exe"
 
 def get_platform_specific_build_args():
     if os.name == "posix":
@@ -54,30 +79,36 @@ def get_platform_specific_build_args():
 
 def get_platform_godot_appdata_dir():
     if os.name == "posix":
-        return f"{os.getenv("HOME")}/.local/share/godot"
+        return f"{os.getenv('HOME')}/.local/share/godot"
     else:
-        return f"{os.getenv("APPDATA")}\\Godot"
+        return f"{os.getenv('APPDATA')}\\Godot"
 
 def run(command):
     return os.system(command) == 0
 
 ##### TEMPLATE HELPER FUNCS #########################
 def get_current_template_dir():
-    return get_platform_godot_appdata_dir() + os.pathsep + "templates" + os.pathsep + VERSION
+    return get_platform_godot_appdata_dir() + os.sep + "templates" + os.sep + VERSION
 
 
 ##### BUILD STEPS #########################
 def build_initial_engine():
+    print_banner("Building Vanilla Engine...")
+
     if not run(f"scons {get_platform_specific_build_args()} tools=yes module_mono_enabled=yes mono_glue=no bits=64 -j16"):
         print("Error building vanilla error!")
         exit(1)
 
 def build_glue():
+    print_banner("Building Mono Glue Code...")
+
     if not run(f"{get_godot_tools_exe_name()} --generate-mono-glue modules/mono/glue"):
         print("Error generating glue code!")
         exit(1)
 
 def build_final_engine():
+    print_banner("Building Final Mono-Enabled Engine...")
+
     if not run(f"scons {get_platform_specific_build_args()} tools=yes module_mono_enabled=yes mono_glue=yes bits=64 -j16"):
         print("Error building final editor!")
         exit(1)
@@ -88,7 +119,8 @@ def build_final_engine():
 def build_templates():
     # Delete and re-create the template dir
     template_dir = get_current_template_dir()
-    shutil.rmtree(template_dir)
+    shutil.rmtree(template_dir, ignore_errors=True)
+    os.mkdir(template_dir)
 
     build_template_debug()
     build_template_release()
@@ -96,56 +128,49 @@ def build_templates():
 
 
 def build_template_debug():
-    if not run(f"scons {get_platform_specific_build_args()}} tools=no module_mono_enabled=yes mono_glue=yes target=release_debug bits=64 -j16"):
+    if not run(f"scons {get_platform_specific_build_args()} tools=no module_mono_enabled=yes mono_glue=yes target=release_debug bits=64 -j16"):
         print("Error building DEBUG template!")
         exit(1)
 
-    #cp bin/godot.x11.opt.debug.64.llvm.mono "$HOME/.local/share/godot/templates/$VERSION"
-    #mv "$HOME/.local/share/godot/templates/$VERSION/godot.x11.opt.debug.64.llvm.mono" "$HOME/.local/share/godot/templates/$VERSION/linux_x11_64_debug"
-    #cp -R bin/data.mono.x11.64.release_debug "$HOME/.local/share/godot/templates/$VERSION/data.mono.x11.64.release_debug"
-
-    #copy bin\godot.windows.opt.debug.64.mono.exe "%APPDATA%\Godot\templates\%VERSION%\windows_64_debug.exe"
-    #robocopy bin\data.mono.windows.64.release_debug "%APPDATA%\Godot\templates\%VERSION%\data.mono.windows.64.release_debug" /E /is /it
-
+    shutil.copy(get_godot_debug_template_exe_name(), get_platform_godot_appdata_dir() + os.sep + "templates" + os.sep + VERSION + os.sep + get_godot_template_root_name() + "_64_debug" + get_godot_template_exe_extension());
+    shutil.copytree(get_godot_debug_data_name(), get_platform_godot_appdata_dir() + os.sep + "templates" + os.sep + VERSION + os.sep + f"data.mono.{get_godot_platform_name()}.64.release_debug");
 
 
 def build_template_release():
-    if not run(f"scons {get_platform_specific_build_args()}} tools=no module_mono_enabled=yes mono_glue=yes target=release bits=64 -j16"):
+    if not run(f"scons {get_platform_specific_build_args()} tools=no module_mono_enabled=yes mono_glue=yes target=release bits=64 -j16"):
         print("Error building DEBUG template!")
         exit(1)
     
-    #cp bin/godot.x11.opt.64.llvm.mono "$HOME/.local/share/godot/templates/$VERSION"
-    #mv "$HOME/.local/share/godot/templates/$VERSION/godot.x11.opt.64.llvm.mono" "$HOME/.local/share/godot/templates/$VERSION/linux_x11_64_release"
-    #cp -R bin/data.mono.x11.64.release "$HOME/.local/share/godot/templates/$VERSION"
-
-    #copy bin\godot.windows.opt.64.mono.exe "%APPDATA%\Godot\templates\%VERSION%\windows_64_release.exe"
-    #robocopy bin\data.mono.windows.64.release "%APPDATA%\Godot\templates\%VERSION%\data.mono.windows.64.release" /E /is /it
+    shutil.copy(get_godot_release_template_exe_name(), get_platform_godot_appdata_dir() + os.sep + "templates" + os.sep + VERSION + os.sep + get_godot_template_root_name() + "_64_release" + get_godot_template_exe_extension());
+    shutil.copytree(get_godot_release_data_name(), get_platform_godot_appdata_dir() + os.sep + "templates" + os.sep + VERSION + os.sep + f"data.mono.{get_godot_platform_name()}.64.release");
 
 
 
 ##### ENTRY POINT #########################
-def main(action):
-    if action == "full":
+def main(command):
+    if command == "full":
         build_initial_engine()
         build_glue()
         build_final_engine()
-    elif action == "glue":
+    elif command == "glue":
         build_glue()
         build_final_engine()
-    elif action == "quick":
+    elif command == "quick":
         build_final_engine()
-    elif action == "templates":
+    elif command == "templates":
         build_templates()
     else:
+        print("Unknown command: ", command)
         usage()
 
 if __name__ == "__main__":
-    action = "glue"
+    if len(sys.argv) == 1:
+        print_banner("Command: ", DEFAULT_COMMAND.upper())
+        main(DEFAULT_COMMAND)
 
-    if len(sys.argv) > 1:
-        action = sys.argv[1]
-        
-    main(action)
+    for i in range(1, len(sys.argv)):
+        print_banner("Command: " + sys.argv[i].upper())
+        main(sys.argv[i])
 
     if os.name == "nt":
         input()
