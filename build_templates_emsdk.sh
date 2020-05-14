79d81d0a7671d5446d7d7ed0328610c6c5a4b4b0
Joe Flood <joe@tinygoose.co.uk>
2020-05-14 15:52:48 +0100

    Add emsdk

--- build_templates_emsdk.sh
+++ build_templates_emsdk.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-VERSION=3.2.1.rc.mono
+VERSION=3.2.2.rc.mono
 
 echo **************************************************
 echo Preparing to build export templates for Emscripten...
@@ -8,10 +8,27 @@ echo **************************************************
 
 rm -rf ~/.godot/templates/$VERSION
 
+# Install the Emscripten SDK
+git clone --depth=1 "https://github.com/juj/emsdk.git" "emsdk"
+cd emsdk
+./emsdk install latest
+./emsdk activate latest
+export EMSCRIPTEN_ROOT
+EMSCRIPTEN_ROOT="$(em-config EMSCRIPTEN_ROOT || true)"
+cd ..
+
+# Build HTML5 export template
+scons platform=javascript tools=no target="$scons_target" \
+      "${SCONS_FLAGS[@]}"
+
+# Move HTML5 export template to the artifacts directory
+mv "$GODOT_DIR/bin"/godot.javascript.*.zip \
+    "$ARTIFACTS_DIR/templates/webassembly_$target.zip"
+
 echo **************************************************
 echo Building DEBUG template for Emscripten...
 echo **************************************************
-scons platform=javascript tools=no module_mono_enabled=yes mono_glue=yes mono_prefix=/etc/mono target=release_debug -j16
+scons platform=javascript tools=no module_mono_enabled=yes mono_glue=yes target=release_debug -j16
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
